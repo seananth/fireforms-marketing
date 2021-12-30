6be679c38c33e2aa9c0e6538672b7083a2fe1771
seananth <seansafstrom@gmail.com>
2021-12-30 14:16:59 +0100

    fix

--- components/landing/Pricing.js
+++ components/landing/Pricing.js
@@ -3,17 +3,17 @@ import BG from "../../public/assets/branding/patternBG.png";
 
 export default function Pricing() {
   return (
-    <section className="flex flex-col mt-24">
-      <div className="flex flex-col items-center">
+    <section className="flex flex-col my-24">
+      <div className="flex flex-col items-center pb-8">
         <p className="text-2xl text-fireGreen font-bold mb-2">Plans</p>
         <p className="text-4xl font-bold">Choose Your Pricing Plan</p>
       </div>
       {/* monthly */}
-      <div className="flex flex-col xl:flex-row xl:justify-center items-center mt-8">
+      <div className="flex flex-col space-y-6 xl:space-y-0 xl:flex-row xl:justify-center items-center mt-8">
         <div className="xl:mr-5 flex flex-col items-center px-24 py-8 border rounded-3xl hover:scale-105 hover:cursor-pointer">
           <div className="text-center">
             <p className="text-4xl">Early Bird Monthly</p>
-            <p className="text-xl py-1 text-fireGreen mt-2">
+            <p className="text-xl py-1 text-fireGreen mt-2 font-bold">
               Limited time only!
             </p>
           </div>
@@ -33,16 +33,16 @@ export default function Pricing() {
           </button>
         </div>
         {/* Yearly */}
-        <div className="xl:mr-5 flex flex-col items-center px-24 py-8 border rounded-3xl hover:scale-105 hover:cursor-pointer">
+        <div className="xl:mr-5 flex flex-col items-center px-28 py-8 border rounded-3xl hover:scale-105 hover:cursor-pointer">
           <div className="text-center">
-            <p className="text-4xl">Early Bird Monthly</p>
+            <p className="text-4xl">Early Bird Yearly</p>
             <p className="text-xl py-1 text-fireGreen mt-2 font-bold">
               2 Months for Free!
             </p>
           </div>
           <div className="flex items-center justify-center py-10">
             <p className="font-bold text-3xl">€49</p>
-            <p className="text-xl">/Monthly</p>
+            <p className="text-xl">/Yearly</p>
           </div>
           <ul className="text-left list-none text-xl space-y-3">
             <li>Drag & Drop Builder</li>
@@ -55,8 +55,7 @@ export default function Pricing() {
             Create Account
           </button>
         </div>
-          
-        </div>
+      </div>
     </section>
   );
 }
