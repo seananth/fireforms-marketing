c74130de78d796714a736af22074438c85af2e1d
seananth <seansafstrom@gmail.com>
2021-12-30 21:43:54 +0100

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/index.css'
+import { useEffect } from "react";
+import Router from "next/router";
+import { initGA, logPageView } from "../analytics/index";
+
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on("routeChangeComplete", logPageView);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
