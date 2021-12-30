0e9be0f4125a304ee584b3edbdcbea207ff2d709
seananth <seansafstrom@gmail.com>
2021-12-30 18:10:01 +0100

    copy

--- components/landing/Pricing.js
+++ components/landing/Pricing.js
@@ -5,12 +5,14 @@ export default function Pricing() {
   return (
     <section className="flex flex-col my-24">
       <div className="flex flex-col items-center pb-8">
-        <p className="text-2xl text-fireGreen font-bold mb-2">Plans</p>
+        <p className="text-2xl text-fireGreen font-bold mb-2">
+          EARLY BIRD SALE
+        </p>
         <p className="text-4xl font-bold">Choose Your Pricing Plan</p>
       </div>
-      {/* monthly */}
       <div className="flex flex-col space-y-6 xl:space-y-0 xl:flex-row xl:justify-center items-center mt-8">
-        <div className="xl:mr-5 flex flex-col items-center px-24 py-8 border rounded-3xl hover:scale-105 hover:cursor-pointer">
+        {/* monthly */}
+        <div className="shadow hover:shadow-sm xl:mr-10 flex flex-col items-center px-24 py-8 border rounded-3xl hover:scale-105 hover:cursor-pointer">
           <div className="text-center">
             <p className="text-4xl">Early Bird Monthly</p>
             <p className="text-xl py-1 text-fireGreen mt-2 font-bold">
@@ -24,8 +26,8 @@ export default function Pricing() {
           <ul className="text-left list-none text-xl space-y-3">
             <li>Drag & Drop Builder</li>
             <li>Simple embed on any site</li>
-            <li>Unlimited - active forms</li>
-            <li>Unlimited - responses/mo</li>
+            <li>Unlimited - Active Forms</li>
+            <li>10,000 - Responses/Mo</li>
             <li>GDPR Complient</li>
           </ul>
           <button className="mt-8 bg-fireGreen p-4 rounded-2xl shadow-sm text-white font-bold ">
@@ -33,7 +35,7 @@ export default function Pricing() {
           </button>
         </div>
         {/* Yearly */}
-        <div className="xl:mr-5 flex flex-col items-center px-28 py-8 border rounded-3xl hover:scale-105 hover:cursor-pointer">
+        <div className="shadow hover:shadow-sm xl:ml-10 flex flex-col items-center px-28 py-8 border rounded-3xl hover:scale-105 hover:cursor-pointer">
           <div className="text-center">
             <p className="text-4xl">Early Bird Yearly</p>
             <p className="text-xl py-1 text-fireGreen mt-2 font-bold">
@@ -47,8 +49,8 @@ export default function Pricing() {
           <ul className="text-left list-none text-xl space-y-3">
             <li>Drag & Drop Builder</li>
             <li>Simple embed on any site</li>
-            <li>Unlimited - active forms</li>
-            <li>Unlimited - responses/mo</li>
+            <li>Unlimited - Active Forms</li>
+            <li>10,000 - Responses/Mo</li>
             <li>GDPR Complient</li>
           </ul>
           <button className="mt-8 bg-fireGreen p-4 rounded-2xl shadow-sm text-white font-bold ">
