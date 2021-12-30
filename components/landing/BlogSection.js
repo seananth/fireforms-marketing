32d8a5a6a73f272042026701e4aebda3bb791a60
seananth <seansafstrom@gmail.com>
2021-12-30 18:34:46 +0100

    icons

--- components/landing/BlogSection.js
+++ components/landing/BlogSection.js
@@ -1,13 +1,18 @@
 import Link from "next/link";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function BlogSection() {
   return (
     <section className="pt-10">
       <div className="bg-fireGreen py-28 flex justify-center">
         <Link href="/blog">
-          <p className="font-bold text-4xl text-white cursor-pointer">
-            Learn more on our blog
-          </p>
+          <div className="flex items-end text-white cursor-pointer">
+            <p className="font-bold text-4xl">Learn more on our blog</p>
+            <div className="ml-4 w-8 flex items-center">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+          </div>
         </Link>
       </div>
     </section>
--- components/landing/FeatureSection.js
+++ components/landing/FeatureSection.js
@@ -1,19 +1,22 @@
-import Image from "next/image";
+import { faChartLine } from "@fortawesome/free-solid-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faHeadset } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function FeatureSection() {
   const data = [
     {
-      icon: "icon",
+      icon: faChartLine,
       title: "More leads",
       sub: "Fireforms was built with conversion rate in mind. Multi-step forms convert up to 2 times higher!",
     },
     {
-      icon: "icon",
+      icon: faCode,
       title: "No code",
       sub: "Hiring developers is expensive and time consuming, with Fireforms we've done the hard work for you.",
     },
     {
-      icon: "icon",
+      icon: faHeadset,
       title: "Friendly support",
       sub: "Our support team is ready to help you in any way to make sure you get the most out of Fireforms!",
     },
@@ -24,8 +27,13 @@ export default function FeatureSection() {
       <div className="bg-fireGreen flex flex-col xl:flex-row xl:space-x-32 justify-center items-center">
         {data.map((feature, index) => {
           return (
-            <div key={index} className="text-center text-white max-w-md py-32 px-8">
-              <p>{feature.icon}</p>
+            <div
+              key={index}
+              className="flex flex-col items-center text-center text-white max-w-md py-28 px-8"
+            >
+              <div className="w-24 flex justify-center mb-2">
+                <FontAwesomeIcon icon={feature.icon} />
+              </div>
               <p className="text-2xl font-bold py-2">{feature.title}</p>
               <p className="text-xl">{feature.sub}</p>
             </div>
