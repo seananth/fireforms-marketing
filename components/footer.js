2e6b3e4cbb252255d5551a601e7c508a984941a6
seananth <seansafstrom@gmail.com>
2021-12-31 11:51:49 +0100

    link to tos

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,11 @@
 import Container from "./container";
 import Image from "next/image";
+import Link from "next/link";
 
 import logo from "../public/assets/branding/fireforms-logo.png";
 
 export default function Footer() {
-  const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
 
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
@@ -14,10 +15,12 @@ export default function Footer() {
             <Image src={logo} />
           </div>
           <div className="mb-4">
-            <a href="#">Home</a>
-            <a href="#" className="mx-4">
-              Terms of Service
-            </a>
+            <a href="/">Home</a>
+            <Link href="/terms">
+              <a className="mx-4">
+                Terms of Service
+              </a>
+            </Link>
             <a href="#">Support</a>
           </div>
           <div>copyright {year} fireforms AB</div>
