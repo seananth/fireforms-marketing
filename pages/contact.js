c64261dddfa96c18c3390c3c96abd82fcfa07b30
seananth <seansafstrom@gmail.com>
2021-12-31 11:55:19 +0100

    contact page

---
+++ pages/contact.js
@@ -0,0 +1,27 @@
+import Container from "../components/container";
+import Layout from "../components/layout";
+import Head from "next/head";
+import Script from "next/script";
+
+export default function terms() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Contact</title>
+        </Head>
+        <Container>
+          <section className="pt-8">
+            {/* fireform goes here */}
+            <div id="fireformsId" name="619a4acbd5e2a7aee408e17c"></div>
+            <Script
+              id="fireforms-script"
+              src="https://app.fireforms.io/embed/Fireforms"
+            />
+            {/* end form */}
+          </section>
+        </Container>
+      </Layout>
+    </>
+  );
+}
