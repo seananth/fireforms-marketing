8c7c1d5b117f0d18cea84ce0413cd8734aa851c3
seananth <seansafstrom@gmail.com>
2022-01-18 19:04:47 +0100

    sitemap the hard way

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,26 +1,37 @@
 import React from "react";
 import fs from "fs";
-// import { getAllPosts } from "../lib/api";
 
 const Sitemap = () => {};
 
 export const getServerSideProps = ({ res }) => {
   const baseUrl = "https://fireforms.io";
-  // const allPosts = getAllPosts(["date", "slug"]);
 
   //dev: "http://localhost:3000"
   //production: "https://fireforms.io",
 
-  const staticPages = fs
-    .readdirSync("/pages")
-    .filter((staticPage) => {
-      return !["_app.js", "_document.js", "sitemap.xml.js"].includes(
-        staticPage
-      );
-    })
-    .map((staticPagePath) => {
-      return `${baseUrl}/${staticPagePath}`;
-    });
+  const staticPages = [
+    "https://fireforms.io/",
+    "https://fireforms.io/contact",
+    "https://fireforms.io/blog",
+  ];
+  const dynamicPages = [
+    {
+      link: "https://fireforms.io/posts/how-to-embed-a-multi-step-form-wordpress",
+      date: "2021-01-09",
+    },
+    {
+      link: "https://fireforms.io/posts/how-to-embed-a-multi-step-form",
+      date: "2021-01-04",
+    },
+    {
+      link: "https://fireforms.io/posts/How-to-Embed-Multi-Step-Form-webflow",
+      date: "2021-01-17",
+    },
+    {
+      link: "https://fireforms.io/posts/Why-You-Should-Use-Multi-Step-Forms",
+      date: "2021-01-02",
+    },
+  ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -30,6 +41,18 @@ export const getServerSideProps = ({ res }) => {
               <url>
                 <loc>${url}</loc>
                 <lastmod>${new Date().toISOString()}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>1.0</priority>
+              </url>
+            `;
+          })
+          .join("")}
+        ${dynamicPages
+          .map((post) => {
+            return `
+              <url>
+                <loc>${post.link}</loc>
+                <lastmod>${post.date}</lastmod>
                 <changefreq>monthly</changefreq>
                 <priority>1.0</priority>
               </url>
@@ -49,16 +72,3 @@ export const getServerSideProps = ({ res }) => {
 };
 
 export default Sitemap;
-
-// ${allPosts
-//   .map((post) => {
-//     return `
-//     <url>
-//       <loc>${baseUrl}/documents/${post.slug}</loc>
-//       <lastmod>${post.date}</lastmod>
-//       <changefreq>monthly</changefreq>
-//       <priority>1.0</priority>
-//     </url>
-//   `;
-//   })
-//   .join("")}
