762a82084d95f48bb93e32f2c156e92ee9fe7c83
seananth <seansafstrom@gmail.com>
2022-02-23 14:40:04 +0100

    basic functionality

--- components/landing/ConversionCalculator.jsx
+++ components/landing/ConversionCalculator.jsx
@@ -1,46 +1,104 @@
 import { useState } from "react";
 
 export default function ConversionCalculator() {
-  const [answer, setAnswer] = useState(false);
+  const [done, setDone] = useState(false);
+  const [answer, setAnswer] = useState({
+    visitors: 0,
+    leads: 0,
+    revenue: 0,
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("nice");
-    setAnswer(true);
+    console.log(answer);
+    setDone(true);
   };
   return (
     <section className="pt-10">
       <div className="bg-fireGreen py-28 justify-center align-center flex flex-col">
         <div className="flex justify-center">
-          {answer === true ? (
-            <p>
-              Your current conversion rate is X% with fireforms you would get Y%
-              - Z%
+          <form
+            action="submit"
+            onSubmit={handleSubmit}
+            className="flex flex-col align-center text-center"
+          >
+            {/* visitors */}
+            <label htmlFor="traffic" className="text-2xl">
+              How Many Visitors Do You Have?
+            </label>
+            <input
+              name="traffic"
+              type="number"
+              onChange={(e) =>
+                setAnswer((answer) => ({
+                  ...answer,
+                  visitors: e.target.value,
+                }))
+              }
+            />
+            <div className="my-2" />
+            {/* leads */}
+            <label htmlFor="leads" className="text-2xl">
+              How many of those turn into leads?
+            </label>
+            <input
+              name="leads"
+              type="number"
+              onChange={(e) =>
+                setAnswer((answer) => ({
+                  ...answer,
+                  leads: e.target.value,
+                }))
+              }
+            />
+            <div className="my-2" />
+            {/* revenue */}
+            <label htmlFor="rev" className="text-2xl">
+              How much revenue are you generating?
+            </label>
+            <input
+              name="rev"
+              type="number"
+              onChange={(e) =>
+                setAnswer((answer) => ({
+                  ...answer,
+                  revenue: e.target.value,
+                }))
+              }
+            />
+            <div className="my-2" />
+            <button type="submit">Submit</button>
+          </form>
+          <div>
+            Current
+            <p className="text-2xl text-white font-bold">
+              conversion rate:{" "}
+              {Math.round((answer.leads / answer.visitors) * 100)}%
             </p>
-          ) : (
-            <form
-              action="submit"
-              onSubmit={handleSubmit}
-              className="flex flex-col align-center"
-            >
-              <div className="flex justify-center">
-                <label htmlFor="traffic">
-                  How much traffic are you getting?
-                </label>
-                <input name="traffic" type="text" />
-              </div>
-              <div className="flex justify-center my-2">
-                <label htmlFor="leads">
-                  How many of those turn into leads?
-                </label>
-                <input name="leads" type="text" />
-              </div>
-              <div className="flex justify-center mb-2">
-                <label htmlFor="rev">How much revenue?</label>
-                <input name="rev" type="text" />
-              </div>
-              <button type="submit">Submit</button>
-            </form>
-          )}
+            <p className="text-2xl text-white font-bold">
+              Leads: {answer.leads}
+            </p>
+            <p className="text-2xl text-white font-bold">
+              Total Revenue: ${answer.revenue}
+            </p>
+            <p className="text-2xl text-white font-bold">
+              Revenue per Lead: ${answer.revenue / answer.leads}
+            </p>
+          </div>
+          <div>
+            With Fireforms
+            <p className="text-2xl text-white font-bold">
+              conversion rate:{" "}
+              {Math.round((answer.leads / answer.visitors) * 100) * 1.5}%
+            </p>
+            <p className="text-2xl text-white font-bold">
+              Leads: {answer.leads * 1.5}
+            </p>
+            <p className="text-2xl text-white font-bold">
+              Total Revenue: $
+              {(answer.revenue / answer.leads) * (answer.leads * 1.5)}
+            </p>
+          </div>
         </div>
       </div>
     </section>
