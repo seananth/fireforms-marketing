18e1ae604e98e90ffbaf44429cab44f5189e01a6
seananth <seansafstrom@gmail.com>
2022-04-01 11:19:38 +0200

    styling

--- components/examples/ExampleCard.jsx
+++ components/examples/ExampleCard.jsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 export const ExampleCard = (props) => {
   return (
     <div>
-      <div className="text-center py-10 h-64 shadow rounded">
-        <Image src={props.preview} />
+      <div className="flex justify-center items-center h-64 shadow rounded hover:shadow-md hover:cursor-pointer">
+        <div className="w-9/12">
+          <Image src={props.preview} />
+        </div>
       </div>
       <div className="pt-2">
-        <p className="text-2xl">{props.title}</p>
+        <p className="text-3xl">{props.title}</p>
         <p className="text-lg">{props.body}</p>
       </div>
     </div>
--- pages/examples.js
+++ pages/examples.js
@@ -1,28 +1,30 @@
 import Container from "../components/container";
 import Layout from "../components/layout";
 import Head from "next/head";
-import image from "../public/assets/examples/lead-gen-example.png";
+import leadGenImage from "../public/assets/examples/lead-gen-example.png";
+import supplierImage from "../public/assets/examples/supplier-example.png";
+import agencyImage from "../public/assets/examples/web-agency-example.png";
 
 //components
 import { ExampleCard } from "../components/examples/ExampleCard";
 
 const exampleData = [
   {
-    preview: image,
-    title: "Lead gen",
-    body: "Modern lead capture form",
+    preview: leadGenImage,
+    title: "Lead generation",
+    // body: "Modern lead capture form",
     link: "/",
   },
   {
-    preview: image,
-    title: "Window cleaner",
-    body: "Booking form for window cleaning businesses",
+    preview: supplierImage,
+    title: "Supplier",
+    // body: "Make ordering supplies a lot easier",
     link: "/",
   },
   {
-    preview: image,
-    title: "Real estate",
-    body: "Learn more about your clients and what their houseing needs are",
+    preview: agencyImage,
+    title: "Margeting Agency",
+    // body: "Learn more about your clients and what their houseing needs are",
     link: "/",
   },
 ];
@@ -34,8 +36,14 @@ export default function Examples() {
         <title>Multi step form examples</title>
       </Head>
       <Container>
-        <p className="py-8 text-4xl text-center">Templates</p>
-        <div className="grid grid-cols-3 gap-8">
+        <div className="bg-fireGreen p-12 mt-2 mb-10 text-white">
+          <p className="pt-8 text-4xl font-bold">Form Examples</p>
+          <p className="pb-8 text-2xl font-medium">
+            Dont feel like starting from scratch? <br /> Choose from our many
+            templates and edit them as you'd like
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
           {exampleData.map((item) => {
             return (
               <ExampleCard
