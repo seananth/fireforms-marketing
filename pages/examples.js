1e2bdec4d547700e0b591752f2f21a972208aa9d
seananth <seansafstrom@gmail.com>
2022-04-01 10:27:51 +0200

    map over examples

--- pages/examples.js
+++ pages/examples.js
@@ -6,45 +6,45 @@ import image from "../public/assets/examples/lead-gen-example.png";
 //components
 import { ExampleCard } from "../components/examples/ExampleCard";
 
+const exampleData = [
+  {
+    preview: image,
+    title: "Lead gen",
+    body: "Modern lead capture form",
+    link: "/",
+  },
+  {
+    preview: image,
+    title: "Window cleaner",
+    body: "Booking form for window cleaning businesses",
+    link: "/",
+  },
+  {
+    preview: image,
+    title: "Real estate",
+    body: "Learn more about your clients and what their houseing needs are",
+    link: "/",
+  },
+];
+
 export default function Examples() {
   return (
     <Layout>
       <Head>
-        <title>Fireforms</title>
+        <title>Multi step form examples</title>
       </Head>
       <Container>
         <p className="py-8 text-4xl text-center">Templates</p>
         <div className="grid grid-cols-3 gap-8">
-          <ExampleCard
-            preview={image}
-            title="Lead gen"
-            body="Modern lead capture form"
-          />
-          <ExampleCard
-            preview={image}
-            title="Window cleaner"
-            body="Booking form for window cleaning businesses"
-          />
-          <ExampleCard
-            preview={image}
-            title="Real estate"
-            body="Learn more about your clients and what their houseing needs are"
-          />
-          <ExampleCard
-            preview={image}
-            title="Lead gen"
-            body="Modern lead capture form"
-          />
-          <ExampleCard
-            preview={image}
-            title="Window cleaner"
-            body="Booking form for window cleaning businesses"
-          />
-          <ExampleCard
-            preview={image}
-            title="Real estate"
-            body="Learn more about your clients and what their houseing needs are"
-          />
+          {exampleData.map((item) => {
+            return (
+              <ExampleCard
+                preview={item.preview}
+                title={item.title}
+                body={item.body}
+              />
+            );
+          })}
         </div>
       </Container>
     </Layout>
